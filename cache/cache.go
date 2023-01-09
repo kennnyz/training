1b2e5b8997158f01f1f99c8ed68a8552e122d343
Muhammed Muhtarov <muhammed1410@yandex.ru>
2023-01-09 15:13:56 +0300

    C

--- cache/cache.go
+++ cache/cache.go
@@ -1,21 +1,62 @@
 package cache
 
+import (
+	"sync"
+	"time"
+)
+
+type Item struct {
+	value      interface{}
+	expiration time.Time
+}
+
 type Cache struct {
-	myMap map[string]any
+	myMap map[string]Item
+	sync  *sync.RWMutex
 }
 
 func New() *Cache {
-	return &Cache{make(map[string]any)}
+	cache := Cache{
+		make(map[string]Item),
+		new(sync.RWMutex),
+	}
+	go CacheClean(&cache)
+	return &cache
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.myMap[key] = value
+func (c *Cache) Set(key string, value interface{}, deadline time.Duration) {
+	c.myMap[key] = Item{
+		value:      value,
+		expiration: time.Now().Add(deadline),
+	}
 }
 
-func (c Cache) Get(key string) any {
-	return c.myMap[key]
+func (c Cache) Get(key string) interface{} {
+	c.sync.RLock()
+	defer c.sync.RUnlock()
+	cacheItem := c.myMap[key]
+	return cacheItem.value
 }
 
 func (c *Cache) Delete(key string) {
+	c.sync.Lock()
 	delete(c.myMap, key)
+	c.sync.Unlock()
+}
+
+func CacheClean(cache *Cache) {
+	for {
+		for key, value := range cache.myMap {
+			if isExpired(value.expiration) {
+				cache.Delete(key)
+			}
+		}
+	}
+}
+
+func isExpired(expiration time.Time) bool {
+	if time.Now().After(expiration) {
+		return true
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -3,23 +3,16 @@ package main
 import (
 	"fmt"
 	"gitgub.com/kennnyz/training/cache"
+	"time"
 )
 
 func main() {
-	//Setting a new cache where we can contain everything of type key(string)-value(any)
 	cache := cache.New()
 
-	// Here I set to my cache in key "UserId" value - true
-	cache.Set("UserId", true)
-
-	// Here is another one example but with int value
-	cache.Set("Muhammed", 14)
-	cache.Set("KOla", 13)
-
-	fmt.Println(cache)
+	cache.Set("Muhammed", true, time.Second*3)
 	fmt.Println(cache.Get("Muhammed"))
 
-	//Deleting record by key
-	cache.Delete("Muhammed")
-	fmt.Println(cache)
+	time.Sleep(4 * time.Second)
+	fmt.Println(cache.Get("Muhammed"))
+
 }
